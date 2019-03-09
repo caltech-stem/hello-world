db7d6a8f2793ee297263ff62c8c948ecdf81b6e9
Roman Tumaykin <roman@tumaykin.com>
2019-03-09 14:19:20 -0800

    With the name in the path

--- HelloWorldApp/HelloWorldApp/urls.py
+++ HelloWorldApp/HelloWorldApp/urls.py
@@ -1,4 +1,4 @@
-"""helloworld URL Configuration
+"""HelloWorldApp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include ('HelloWorldPagesApp.urls'))
 ]
