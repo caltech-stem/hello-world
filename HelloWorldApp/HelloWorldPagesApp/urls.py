2f82868e316d3870a5dba1acc9ab27a328c34561
Roman Tumaykin <roman@tumaykin.com>
2019-03-30 14:05:12 -0700

    Mar 30 commit

--- HelloWorldApp/HelloWorldPagesApp/urls.py
+++ HelloWorldApp/HelloWorldPagesApp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<name>/', views.hello, name='hello'),
-
+    path('<firstName>/', views.hello, name='hello'),
+    path('<firstName>/answer/', views.answer, name='answer'),
 ]
--- HelloWorldApp/HelloWorldPagesApp/views.py
+++ HelloWorldApp/HelloWorldPagesApp/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, this is my first application Here")
+    template = loader.get_template('HelloWorldPages/index.html')
+    context = {
+        'name': "test",
+    }
+    return HttpResponse(template.render(context, request))
 
-def hello(request, name):
-    return HttpResponse("Hello, %s" % name)
+def hello(request, firstName):
+    template = loader.get_template('HelloWorldPagesApp/index.html')
+    context = {
+        'name': firstName,
+    }
+    return HttpResponse(template.render(context, request))
+
+def answer(request, firstName):
+    answer = request.POST['answer']
+    template = loader.get_template('HelloWorldPagesApp/answer.html')
+    context = {
+        'name': firstName,
+        'answer': answer
+    }
+    return HttpResponse(template.render(context, request))
